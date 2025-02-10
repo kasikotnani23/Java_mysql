87da774ade6805287267eaf09d7b7d5a773f29eb
kasikotnani23 <107323896+kasikotnani23@users.noreply.github.com>
2025-02-10 14:51:19 +0530

    Create JavaMysqlApplication.java

---
+++ src/main/java/com/example/JavaMysqlApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMysqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMysqlApplication.class, args);
+    }
+}
